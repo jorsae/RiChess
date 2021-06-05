2499e620a848d819e65fcaeec5ea83ade4276d9a
jorgen saeter <jorgen.sater@gmail.com>
2021-06-06 08:18:35 +1000

    changed board style to row major

--- src/interface/iboard.py
+++ src/interface/iboard.py
@@ -4,20 +4,20 @@ from model import Colour
 from typing import List, Optional
 
 class IBoard(metaclass=ABCMeta):
-    def __init__(self, files: int = 8, ranks: int = 8):
-        self.files = files
+    def __init__(self, ranks: int = 8, files: int = 8, ):
         self.ranks = ranks
-        self.board = [[None for i in range(self.files)] for j in range(self.ranks)]
+        self.files = files
+        self.board = [[None for i in range(self.ranks)] for j in range(self.files)]
     
-    def get_piece(self, file, rank):
-        return self.board[file][rank]
+    def get_piece(self, rank, file):
+        return self.board[rank][file]
 
     def __str__(self):
         output = ''
-        for file in range(self.files):
+        for rank in range(self.ranks):
             line = ''
-            for rank in range(self.ranks):
-                piece = self.get_piece(file, rank)
+            for file in range(self.files):
+                piece = self.get_piece(rank, file)
                 if piece == None:
                     line += ' 0 |'
                 else:
--- src/model/board.py
+++ src/model/board.py
@@ -1,4 +1,7 @@
 from interface import IBoard
 
+# rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1
+
 class Board(IBoard):
+    # def setup_board(self, fen):
     pass
