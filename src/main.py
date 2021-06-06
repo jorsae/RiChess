e756ab4c3efca5360200a4bc112d22fbe54c1b8e
jorgen saeter <jorgen.sater@gmail.com>
2021-06-07 07:12:17 +1000

    Added sample code to test movement

--- src/main.py
+++ src/main.py
@@ -2,6 +2,22 @@ from model.parser import *
 from model.piece import *
 from model.game import *
 
+
+fp = FenParser("rnbqkbnr/pppppppp/8/8/4K3/8/PPPPPPPP/RNBQ1B1R w kq - 0 1")
+fp.parse()
+chess_game = ChessGame()
+chess_game.variant.load_rules()
+chess_game.board.load_from_fen(fp)
+
+rank = 4
+file = 4
+
+piece = chess_game.board.get_piece(rank, file)
+print(piece.name)
+print(chess_game.board.get_available_moves(rank, file))
+print(chess_game.board)
+
+"""
 fp = FenParser("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
 fp.parse()
 chess_game = ChessGame()
@@ -9,7 +25,6 @@ chess_game.variant.load_rules()
 chess_game.board.load_from_fen(fp)
 
 print(chess_game.board)
-
 print('bishop')
 print(chess_game.board.get_available_moves(2, 7))
 print('rook')
@@ -22,3 +37,4 @@ print('king')
 print(chess_game.board.get_available_moves(4, 0))
 print('pawn')
 print(chess_game.board.get_available_moves(0, 6))
+"""
