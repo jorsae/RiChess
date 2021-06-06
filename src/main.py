d9182e650b57ae99a9207e09f268b5f137ea2985
jorgen saeter <jorgen.sater@gmail.com>
2021-06-07 00:15:08 +1000

    Cleaned up main.py

--- src/main.py
+++ src/main.py
@@ -4,7 +4,8 @@ from model.game import *
 
 board = Board()
 
-fp = FenParser("1r1qkb1r/p3ppp1/2p1bn1p/8/2B1Pp2/2N1B2P/PP2Q1P1/2R2RK1 w Q g3 19 25")
+fp = FenParser("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
 fp.parse()
 board.load_from_fen(fp)
-print(fp.has_moved)
+print(board)
+print(f'{board.turn} {board.halfmove} : {board.fullmove}')
